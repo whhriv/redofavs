d579387fc781eae8ad192219001db9d7c8538563
William Reeder <whhriv@gmail.com>
2023-10-26 15:56:46 -0700

    finished with Sarah

--- app/routes.py
+++ app/routes.py
@@ -9,13 +9,11 @@ def index():
 def ski_images():
     images = [
         'ski1.jpg',
-        '/templates/ski2.jpg',
-        './templates/ski3.jpg',
-        '/ski4.jpg',
-        '/ski5.jpg',
+        'ski2.jpg',
+        'ski3.jpg',
+        'ski4.jpg',
+        '/ki5.jpg',
     ]
-    return render_template('colors.html', images=images)
+    return render_template('skis.html', images=images)
 
-# @app.route('/skis')
-# def img():
-#     return render_template('skis.html')
+
